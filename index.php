70833c74c498ec760bc03030d7b900325b426c1b
Mohit Dhiman <56352729+Mohitdhiman07@users.noreply.github.com>
2021-01-07 04:05:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,38 +9,7 @@ include_once("sangrah-aboutus.html");
 include_once("sangrah-coding-kids.css");
 include_once("termspolicy.html");
 include_once("type.css");
-include_once("custom.js");
-include_once("theme.init.js");
-include_once("theme.js");
-include_once("demo-seo-2.js");
-include_once("theme.css");
-include_once("theme-shop.css");
-include_once("theme-elements.css");
-include_once("theme-blog.css");
-include_once("custom.css");
-include_once("skin-seo-2.css.css");
-include_once("demo-seo-2.css");
-include_once("animate.compat.css");
-include_once("bootstap.min.css");
-include_once("bootstap.min.js");
-include_once("all.min.css");
-include_once("jquery.min.js");
-include_once("jquery.appear.min.js");
-include_once("jquery.cookie.min.js");
-include_once("jquery.easing.min.js");
-include_once("jquery.easypiechart.min.js");
-include_once("jquery.gmap.min.js");
-include_once("jquery.gmap.min.js");
-include_once("jquery.lazyload.min.js");
-include_once("jquery.validate.min.js");
-include_once("jquery.magnific-popup.min.js");
-include_once("magnific-popup.min.css");
-include_once("modernizr.min.js");
-include_once("owl.carousel.min.css");
-include_once("owl.theme.default.min.css");
-include_once("owl.carousel.min.js");
-include_once("popper.min.js");
-include_once("simple-line-icons.min.css");
+
 
 
 
